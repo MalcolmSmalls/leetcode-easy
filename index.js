34045ce348fa671103654687c115542c1f249cd4
MalcolmSmalls <smallsbeats@gmail.com>
2023-01-19 15:55:57 -0800

    Finish LeetCode 2: 1920. Build Array from Permutation

--- index.js
+++ index.js
@@ -3,3 +3,11 @@
 var getConcatenation = function(nums) {
     return [...nums, ...nums]
 };
+
+
+
+// 20. Build Array from Permutation
+
+var buildArray = function(nums) {
+    return nums.map((item, index) => nums[nums[index]])
+};
