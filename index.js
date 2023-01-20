8136ca46e9a9383cca1b9f811c2cc5b680031337
MalcolmSmalls <smallsbeats@gmail.com>
2023-01-19 16:14:20 -0800

    Finish 344. Reverse String

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // 1929. Concatenation of Array
 
-var getConcatenation = function(nums) {
-    return [...nums, ...nums]
+// var getConcatenation = function(nums) {
+//     return [...nums, ...nums]
+// };
+
+
+
+// 1920. Build Array from Permutation
+
+// var buildArray = function(nums) {
+//     return nums.map((item, index) => nums[nums[index]])
+// };
+
+
+// 344. Reverse String
+
+var reverseString = function(s) {
+    s.reverse()
 };
 
-
-
-// 20. Build Array from Permutation
-
-var buildArray = function(nums) {
-    return nums.map((item, index) => nums[nums[index]])
-};
+console.log(reverseString(["h","e","l","l","o"]))
