c42e8b5177c7e32e61a360e81f5f18ff2b2ae63e
MalcolmSmalls <smallsbeats@gmail.com>
2023-03-11 11:22:09 -0800

    Finish twoSum challenge

--- interviewQuestions.js
+++ interviewQuestions.js
@@ -18,3 +18,19 @@ var detectCycle = function (head) {
   }
   return slow
 }
+
+// twoSum
+
+function twoSum(arr, target) {
+  const map = new Map()
+  for (let i = 0; i < arr.length; i++) {
+    let num1 = arr[i]
+    let num2 = target - num1
+    if (map.has(num2)) {
+      return [i, map.get(num2)]
+    }
+    map.set(num1, i)
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9))
